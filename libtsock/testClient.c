f69791aeeb49bc13f9c03a051d3cc9c079e06920
Julien Marchand <contact@julien-marchand.fr>
2012-02-07 19:28:08 +0100

    libtsock fonctionne (on était mauvais en fait : listen() avec du SOCK_DGRAM...)

--- libtsock/testClient.c
+++ libtsock/testClient.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "tsock.h"
 
-int main () {
-    char buffer [4];
-    int tsockClient = tsockConnect("./lol");
-    tsockRead(tsockClient, buffer, (size_t) sizeof(buffer));
+int main() {
+    char buffer[4];
+    int tsockClient = tsock_connect("lol.sock");
+    tsock_read(tsockClient, buffer, (size_t) sizeof(buffer));
     printf("%s\n",buffer);
-    tsockClose (tsockClient);
+    tsock_close(tsockClient);
+
     return 0;
 }
     
