9297c36351b39af7299f93ee13478a037b67132a
Adrien Sulpice <adrien.sulpice@gmail.com>
2012-02-14 16:59:04 +0100

    Ajout message création de fenêtre

--- emu/libs/pronlib/pronlib_events.h
+++ emu/libs/pronlib/pronlib_events.h
@@ -3,13 +3,42 @@
 
 
 #include <pron_messages.h>
+#include <stdlib.h>
 
 
-struct PronEvents : public PronMessage {
+struct PronEvent : public PronMessage {
 	/** Constructor. */
-	PronEvents(MessageType type)
+	PronEvent(MessageType type)
 	    : PronMessage(type) {
 	}
+	
 };
 
+
+/**
+ * ClearWindow request.
+ * Sent by a client to clear the contents of a window.
+ */
+struct EventWindowCreated : public PronEvent {
+  /** Constructor. */
+  EventWindowCreated()
+       : PronEvent(EVENT_WINDOW_CREATED) {
+  }
+  EventWindowCreated(PronWindowAttributes attributes)
+       : PronEvent(EVENT_WINDOW_CREATED) {
+     this->attributes = attributes;
+  }
+
+  PronWindowAttributes attributes;
+};
+
+
+PronEvent * getPronEvent() {
+  int size = sizeof(EventWindowCreated);
+  
+  return (PronEvent *) malloc(size);
+}
+
+
 #endif // __PRONLIB_EVENTS_H__
+
