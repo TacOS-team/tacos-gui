3cfee9d811f189e886dfdaa4e2f6a961de6f4008
Julien Marchand <contact@julien-marchand.fr>
2012-02-15 02:09:39 +0100

    Réindentation tsock

--- emu/libs/libtsock/tsock.c
+++ emu/libs/libtsock/tsock.c
@@ -10,100 +10,100 @@
 #include "tsock.h"
 
 int tsock_listen(const char *path) {
-    int tsockServ = 0;
-    struct sockaddr_un addr;
-    socklen_t addrLen = 0;
+  int tsockServ = 0;
+  struct sockaddr_un addr;
+  socklen_t addrLen = 0;
 
-    // Paramétrage de l'adresse
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strcpy(addr.sun_path, path);
+  // Paramétrage de l'adresse
+  memset(&addr, 0, sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path, path);
 
-    addrLen = sizeof(addr);
+  addrLen = sizeof(addr);
 
-    // Création de la socket
-    tsockServ = socket(PF_UNIX, SOCK_SEQPACKET | SOCK_NONBLOCK, 0);
-    if (tsockServ == -1) {
-	perror("socket");
-    }
+  // Création de la socket
+  tsockServ = socket(PF_UNIX, SOCK_SEQPACKET | SOCK_NONBLOCK, 0);
+  if (tsockServ == -1) {
+    perror("socket");
+  }
 
-    // Bind
-    if (bind(tsockServ, (struct sockaddr*) &addr, addrLen) != 0) {
-	perror("bind");
-    }
+  // Bind
+  if (bind(tsockServ, (struct sockaddr*) &addr, addrLen) != 0) {
+    perror("bind");
+  }
 
-    // Listen
-    if (listen(tsockServ, 5) != 0) {
-	perror("listen");
-    }
+  // Listen
+  if (listen(tsockServ, 5) != 0) {
+    perror("listen");
+  }
 
-    return tsockServ;
+  return tsockServ;
 }
 
 int tsock_connect(const char *path) {
-    int tsockClient = 0;
-    struct sockaddr_un addr;
-    socklen_t addrLen = 0;
+  int tsockClient = 0;
+  struct sockaddr_un addr;
+  socklen_t addrLen = 0;
 
-    // Paramétrage de l'adresse 
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strcpy(addr.sun_path,path);
+  // Paramétrage de l'adresse 
+  memset(&addr, 0, sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path,path);
 
-    addrLen = sizeof(addr);
+  addrLen = sizeof(addr);
 
-    // Création de la socket
-    tsockClient = socket(PF_UNIX, SOCK_SEQPACKET/* | SOCK_NONBLOCK*/, 0);
-    if (tsockClient == -1) {
-	perror("socket");
-    }
-    
-    // Connect
-    if (connect(tsockClient, (struct sockaddr*) &addr, addrLen) != 0) {
-	perror("connect");
-	return -1;
-    }
+  // Création de la socket
+  tsockClient = socket(PF_UNIX, SOCK_SEQPACKET/* | SOCK_NONBLOCK*/, 0);
+  if (tsockClient == -1) {
+    perror("socket");
+  }
 
-    return tsockClient;
+  // Connect
+  if (connect(tsockClient, (struct sockaddr*) &addr, addrLen) != 0) {
+    perror("connect");
+    return -1;
+  }
+
+  return tsockClient;
 }
 
 int tsock_accept(int tsockServer) {
-    struct sockaddr_un addrClientOSEF;
-    socklen_t addrLenOSEF = sizeof(addrClientOSEF);
-    int tsockCanal = 0; // socket du canal client <--> serveur
+  struct sockaddr_un addrClientOSEF;
+  socklen_t addrLenOSEF = sizeof(addrClientOSEF);
+  int tsockCanal = 0; // socket du canal client <--> serveur
 
-    tsockCanal = accept(tsockServer, (struct sockaddr*) &addrClientOSEF, &addrLenOSEF);
-    /*if (tsockCanal == -1) {
-	perror("accept");
-    }*/
+  tsockCanal = accept(tsockServer, (struct sockaddr*) &addrClientOSEF, &addrLenOSEF);
+  if (tsockCanal == -1) {
+    //perror("accept");
+  }
 
-    return tsockCanal;
+  return tsockCanal;
 }
 
 ssize_t tsock_read(int tsock, void *buffer, size_t len) {
-    ssize_t nbBytesRcvd = recv(tsock, buffer, len, 0);
+  ssize_t nbBytesRcvd = recv(tsock, buffer, len, 0);
 
-    if (nbBytesRcvd == -1) {
-	//perror("read");
-    }
+  if (nbBytesRcvd == -1) {
+    //perror("read");
+  }
 
-    return nbBytesRcvd;
+  return nbBytesRcvd;
 }
 
 ssize_t tsock_write(int tsock, void * buffer, size_t len) {
-    ssize_t nbBytesSent = send(tsock, buffer, len, 0);
+  ssize_t nbBytesSent = send(tsock, buffer, len, 0);
 
-    if (nbBytesSent == -1) {
-	perror("write");
-	printf("errno = %d, waiting a bit...\n", errno);
-	usleep(1000000);
-    }
+  if (nbBytesSent == -1) {
+    perror("write");
+    printf("errno = %d, waiting a bit...\n", errno);
+    usleep(1000000);
+  }
 
-    return nbBytesSent;
+  return nbBytesSent;
 }
 
 void tsock_close(int tsock) {
-    if (close(tsock) != 0) {
-	perror("close");
-    }
+  if (close(tsock) != 0) {
+    perror("close");
+  }
 }
