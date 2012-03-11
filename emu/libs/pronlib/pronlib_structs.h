54860bd162dd5ca5a99206b9aa267598832504f6
Adrien Sulpice <adrien.sulpice@gmail.com>
2012-03-12 00:40:46 +0100

    Ajout couleur de fond dans les attributs de la pronlib

--- emu/libs/pronlib/pronlib_structs.h
+++ emu/libs/pronlib/pronlib_structs.h
@@ -1,11 +1,14 @@
 #ifndef __PRONLIB_STRUCTS_H__
 #define __PRONLIB_STRUCTS_H__
 
+#include <vesa_types.h>
+
 struct PronWindowAttributes {
   unsigned short x;
   unsigned short y;
   unsigned short width;
   unsigned short height;
+  color_t        bgColor;
 };
 
 #endif // __PRONLIB_STRUCTS_H__
