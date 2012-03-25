ad71669ac8381cbaa732c5120906e576c78a2250
Benoit Morgan <morgan@etud.insa-toulouse.fr>
2012-03-25 16:17:11 +0200

    Ajout des fonctions graphiques a Pixmap Bizzare vu que la classe Drawable a ses fonctions virtuelles pures le commit précédent aurait du gueuler à la compilation ce qui n'est pas le cas... à voir

--- emu/applications/pron/pixmap.cpp
+++ emu/applications/pron/pixmap.cpp
@@ -10,3 +10,13 @@ Pixmap::Pixmap(Screen *screen, int id, Client *creator, int width, int height)
 Pixmap::~Pixmap() {
   free(this->buf);
 }
+
+void Pixmap::drawPoint(int x, int y){/*TODO unimplemented*/}
+void Pixmap::drawLine(int x1, int y1, int x2, int y2){/*TODO unimplemented*/}
+void Pixmap::drawRect(int x1, int y1, int width, int height){/*TODO unimplemented*/}
+void Pixmap::fillRectangle(int x, int y, int width, int height){/*TODO unimplemented*/}
+void Pixmap::putImage(PronImage *image, int x, int y){/*TODO unimplemented*/}
+void Pixmap::drawCircle(int x, int y, int radius){/*TODO unimplemented*/}
+void Pixmap::fillCircle(int x, int y, int radius){/*TODO unimplemented*/}
+void Pixmap::clear(){/*TODO unimplemented*/}
+void Pixmap::clear(int x, int y, int width, int height){/*TODO unimplemented*/}
--- emu/applications/pron/pixmap.h
+++ emu/applications/pron/pixmap.h
@@ -17,6 +17,20 @@ public:
   */
   Pixmap(Screen *screen, int id, Client *creator, int width, int height);
   ~Pixmap();
+
+  /**
+   * Graphic methods
+   */
+  void drawPoint(int x, int y);
+  void drawLine(int x1, int y1, int x2, int y2);
+  void drawRect(int x1, int y1, int width, int height);
+  void fillRectangle(int x, int y, int width, int height);
+  void putImage(PronImage *image, int x, int y);
+  void drawCircle(int x, int y, int radius);
+  void fillCircle(int x, int y, int radius);
+  void clear();
+  void clear(int x, int y, int width, int height);
+
 private:
   // Image buffer
   char *buf;
