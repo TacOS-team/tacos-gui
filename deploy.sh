df9072e0d8eb01eef54ca646619141f669111855
Julien Marchand <contact@julien-marchand.fr>
2012-04-22 01:18:31 +0200

    Début de script de déploiement pour TacOS.

---
+++ deploy.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+tacos_gui_appdir=emu/applications
+tacos_appdir=../applications/gui
+
+tacos_gui_libdir=emu/libs
+tacos_libdir=../libs
+
+applications="pron guacamole pron_cube.cpp pron_cube_mouse.cpp pron_cube_tuning.cpp"
+libs="pronlib"
+
+if [ -f $tacos_gui_appdir/Makefile.tacos ]; then
+  echo "Configuring Makefile for GUI applications..."
+  cp $tacos_gui_appdir/Makefile.tacos $tacos_appdir/Makefile
+else
+  echo "No TacOS Makefile for GUI applications found."
+fi
+
+for app in $applications; do
+  if [ -d $tacos_gui_appdir/$app ]; then
+    if [ -d $tacos_appdir/$app ]; then
+      echo "Deleting $tacos_appdir/$app..."
+      rm -rf $tacos_appdir/$app
+    fi
+
+    echo "Copying $tacos_gui_appdir/$app to $tacos_appdir/$app..."
+    cp -r $tacos_gui_appdir/$app $tacos_appdir/$app
+
+    if [ -f $tacos_appdir/$app/Makefile.tacos ]; then
+      echo "Configuring Makefile..."
+      mv $tacos_appdir/$app/Makefile.tacos $tacos_appdir/$app/Makefile
+    else
+      echo "No TacOS Makefile found for $app."
+    fi
+  elif [ -f $tacos_gui_appdir/$app ]; then
+    echo "Copying $tacos_gui_appdir/$app to $tacos_appdir/$app..."
+    cp $tacos_gui_appdir/$app $tacos_appdir/$app
+  else
+    echo "Application $app does not exist."
+  fi
+done
+
+for lib in $libs; do
+  if [ -d $tacos_gui_libdir/$lib ]; then
+    if [ -d $tacos_libdir/$lib ]; then
+      echo "Deleting $tacos_libdir/$lib..."
+      rm -rf $tacos_libdir/$lib
+    fi
+
+    echo "Copying $tacos_gui_libdir/$lib to $tacos_libdir/$lib..."
+    cp -r $tacos_gui_libdir/$lib $tacos_libdir/$lib
+
+    if [ -f $tacos_libdir/$lib/Makefile.tacos ]; then
+      echo "Configuring Makefile..."
+      mv $tacos_libdir/$lib/Makefile.tacos $tacos_libdir/$lib/Makefile
+    else
+      echo "No TacOS Makefile found."
+    fi
+  else
+    echo "Library $lib does not exist."
+  fi
+done
