ded1dd54ec002a8bf900c7c1030ac16a5398d7cd
Julien Marchand <contact@julien-marchand.fr>
2012-05-01 11:54:32 +0200

    Doc GC

--- emu/applications/pron/gc.h
+++ emu/applications/pron/gc.h
@@ -6,26 +6,58 @@
 
 using namespace pron;
 
+/**
+ * Graphics context.
+ * @todo XXX public members bourrin
+ */
 class GC {
-public:
-  static std::vector<GC*> gcs;
+ public:
+  unsigned int id; /**< GC id */
+  color_t bg; /**< Background color */
+  color_t fg; /**< Foreground color */
+  int font_num; /**< Id of the current font */
 
-  unsigned int id;
-  color_t bg;
-  color_t fg;
-  int font_num;
-
+  /**
+   * Returns the queried graphics context.
+   * @param id The graphics context to get
+   * @return the queried graphics context
+   */
   static GC* getGC(unsigned int id);
 
+  /**
+   * Default constructor.
+   * Creates a graphics context with default values.
+   */
   GC();
 
+  /**
+   * Constructor.
+   * @param id The id of the graphics context
+   * @param values The values to set in the new GC
+   * @param mask The mask that specifies the values to be set
+   */
   GC(unsigned int id, const PronGCValues &values, unsigned int mask);
 
+  /**
+   * Returns the values of the graphics context.
+   * @return the values of the graphics context
+   */
   PronGCValues getValues();
 
+  /** 
+   * Sets the values of the graphics context.
+   * @param values The values to set
+   * @param mask The mask that specifies the values to be set
+   */
   void setValues(const PronGCValues &values, unsigned int mask);
 
+  /**
+   * Destroys the graphics context.
+   */
   void destroy();
+
+ private
+  static std::vector<GC*> gcs; /**< List of all graphics contexts */
 };
 
 #endif
