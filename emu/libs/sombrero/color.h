cc7d334c6464c1d16168e0e4c7c79ef158d807d9
Benoît Morgan <morgan@etud.insa-toulouse.fr>
2012-05-05 19:42:39 +0200

    Ajout des fichiers de la classe color

---
+++ emu/libs/sombrero/color.h
@@ -0,0 +1,21 @@
+#ifndef __COLOR_H__
+#define __COLOR_H__
+/**
+ * @file color.h
+ * Color class definition
+ */
+
+namespace sombrero {
+
+/**
+ * Color class definition
+ * @brief The Color
+ */ 
+class Color {
+ private:
+ public:
+};
+
+} // namespace sombrero
+
+#endif//__COLOR_H__
