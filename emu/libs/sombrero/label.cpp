95c829e6151af6902f62fe7750815847df9ba331
Adrien Sulpice <adrien.sulpice@gmail.com>
2012-05-07 14:14:33 +0200

    Ajout d'un décalage sur la width en fonction du nombre de caractères pour que le texte soit plus ou moins centré

--- emu/libs/sombrero/label.cpp
+++ emu/libs/sombrero/label.cpp
@@ -11,12 +11,13 @@ namespace sombrero {
   Label::~Label() {}
 
   void Label::draw() {
+    //printf("draw label (x, y, w, h) : %d, %d, %d, %d\n", this->getX(), this->getY(), this->getWidth(), this->getHeight());
     // We can clear the window
     pron::pronClearWindow(Application::getInstance()->d, this->pronWindow);
     // Draws the background 
     pron::pronDrawRect(Application::getInstance()->d, this->pronWindow, Application::getInstance()->d->defaultGC, this->getX(), this->getY(), this->getWidth(), this->getHeight());
     // Draws the text
-    pron::pronDrawText(Application::getInstance()->d, this->pronWindow, Application::getInstance()->d->defaultGC, this->getWidth() / 2, this->getHeight() / 2, this->text, strlen(this->text));
+    pron::pronDrawText(Application::getInstance()->d, this->pronWindow, Application::getInstance()->d->defaultGC, this->getWidth() / 2 - strlen(text)/2*5, this->getHeight() / 2, this->text, strlen(this->text));
   }
 
   void Label::setText(char *text) {
