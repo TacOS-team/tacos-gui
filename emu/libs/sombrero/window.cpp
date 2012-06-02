2196f30c40f932b1f03eedfb842dacf6d9d8d96f
Julien Marchand <contact@julien-marchand.fr>
2012-06-02 18:00:12 +0200

    haha coding style olol

--- emu/libs/sombrero/window.cpp
+++ emu/libs/sombrero/window.cpp
@@ -4,55 +4,55 @@
 
 namespace sombrero {
 
-  Window::Window(std::string title, int x, int y, int width, int height,
-      bool decorate)
+Window::Window(std::string title, int x, int y,
+    int width, int height, bool decorate)
     : Bin() {
-      this->setX(x);
-      this->setY(y);
-      this->setWidth(width);
-      this->setHeight(height);
-      // Creates the window
-      pron::PronWindowAttributes attr;
-      attr.x = this->getX();
-      attr.y = this->getY();
-      attr.width = this->getWidth();
-      attr.height = this->getHeight();
-      attr.wm_decorate = decorate;
-      strncpy(attr.wm_title, title.c_str(), WM_TITLE_MAX_LEN);
-      this->pronWindow = pron::pronCreateWindowExtended(
-          Application::getInstance()->d,
-          Application::getInstance()->d->rootWindow,
-          attr, pron::WIN_ATTR_X | pron::WIN_ATTR_Y | pron::WIN_ATTR_WIDTH |
-          pron::WIN_ATTR_HEIGHT | pron::WIN_ATTR_WM_DECORATE |
-          pron::WIN_ATTR_WM_TITLE);
+  this->setX(x);
+  this->setY(y);
+  this->setWidth(width);
+  this->setHeight(height);
+  // Creates the window
+  pron::PronWindowAttributes attr;
+  attr.x = this->getX();
+  attr.y = this->getY();
+  attr.width = this->getWidth();
+  attr.height = this->getHeight();
+  attr.wm_decorate = decorate;
+  strncpy(attr.wm_title, title.c_str(), WM_TITLE_MAX_LEN);
+  this->pronWindow = pron::pronCreateWindowExtended(
+      Application::getInstance()->d,
+      Application::getInstance()->d->rootWindow,
+      attr, pron::WIN_ATTR_X | pron::WIN_ATTR_Y | pron::WIN_ATTR_WIDTH |
+      pron::WIN_ATTR_HEIGHT | pron::WIN_ATTR_WM_DECORATE |
+      pron::WIN_ATTR_WM_TITLE);
 
-      // Associates the pron::window to the widget
-      Application::getInstance()->widgets[this->pronWindow] = this;
-      // Maps the window
-      pron::pronMapWindow(Application::getInstance()->d, this->pronWindow);
-      // Select events
-      this->subscribeEvent(pron::EV_EXPOSE);
-      this->subscribeEvent(pron::EV_DESTROY_WINDOW);
-      this->subscribeEvent(pron::EV_RESIZE_WINDOW);
+  // Associates the pron::window to the widget
+  Application::getInstance()->widgets[this->pronWindow] = this;
+  // Maps the window
+  pron::pronMapWindow(Application::getInstance()->d, this->pronWindow);
+  // Select events
+  this->subscribeEvent(pron::EV_EXPOSE);
+  this->subscribeEvent(pron::EV_DESTROY_WINDOW);
+  this->subscribeEvent(pron::EV_RESIZE_WINDOW);
+}
+
+Window::~Window() {}
+
+void Window::handleEventResizeWindow(int width, int height) {
+  Widget *w = this->getWidget();
+  if(w != NULL) {
+    w->setWidth(width);
+    w->setHeight(height);
+    w->update();
   }
+}
 
-  Window::~Window() {}
-
-  void Window::handleEventResizeWindow(int width, int height) {
-    Widget *w = this->getWidget();
-    if(w != NULL) {
-      w->setWidth(width);
-      w->setHeight(height);
-      w->update();
-    }
-  }
-
-  void Window::add(Widget *widget) {
-    widget->setX(0);
-    widget->setY(0);
-    widget->setWidth(this->getWidth());
-    widget->setHeight(this->getHeight());
-    Bin::add(widget);
-  }
+void Window::add(Widget *widget) {
+  widget->setX(0);
+  widget->setY(0);
+  widget->setWidth(this->getWidth());
+  widget->setHeight(this->getHeight());
+  Bin::add(widget);
+}
 
 } // namespace sombrero
